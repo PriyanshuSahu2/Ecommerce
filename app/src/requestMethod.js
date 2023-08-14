4e23c2ac799e54af198e5dc586d36b1097c57e9b
Priyanshu <sahupriyanshu108@gmail.com>
2023-08-14 12:17:15 +0530

    changes

--- app/src/requestMethod.js
+++ app/src/requestMethod.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BASE_URL = "https://markethub-backend.onrender.com/api";
-export const BASE_URL = "http://localhost:5000/api";
+ export const BASE_URL = "https://markethub-backend.onrender.com/api";
+// export const BASE_URL = "http://localhost:5000/api";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
